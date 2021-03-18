5837b9e3f7227eee65203104bef8d89e61a637b7
MarieDeSmedt <desmedtmarie@outlook.com>
2021-03-18 16:54:24 +0100

    start sample

--- src/d01_data/load_data.py
+++ src/d01_data/load_data.py
@@ -6,16 +6,19 @@ from conf import conf
 db_connect = ut.connect_to_mysql()
 
 # create a df from tsv
-df_to_clean = pd.read_csv(conf.db_local_path, sep='\t', nrows=10000, low_memory=False)
+df = pd.read_csv(conf.db_local_path, sep='\t',  low_memory=False)
+df_to_clean = df.sample(n=10000)
 
-print(ut.info_df(df_to_clean))
+# how many columns?
+print(df_to_clean)
+#163 columns
+
+# show empties columns (if sum=10000)
+print(df_to_clean.isnull().sum())
+
+#dataframe.drop(columns=['nomdelacolonne', 'nomdelacolonne'])
 
 
-#save data to clean
+# save data to clean
 df_name = "food_fact"
 ut.save_to_mysql(db_connect, df_to_clean, df_name)
-
-
-
-
-
