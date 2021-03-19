caf9eba527e20fa1d5df3de34cbd038fbb26f7bc
MarieDeSmedt <desmedtmarie@outlook.com>
2021-03-19 16:46:59 +0100

    start data_cleaning

--- conf/conf.py
+++ conf/conf.py
@@ -6,3 +6,4 @@ database_name = "food_flix"
 
 # localisation of the db
 db_local_path = "/home/apprenant/Downloads/archive/en.openfoodfacts.org.products.tsv"
+
--- src/d01_data/load_data.py
+++ src/d01_data/load_data.py
@@ -9,5 +9,7 @@ db_connect = ut.connect_to_mysql()
 df = pd.read_csv(conf.db_local_path, sep='\t', low_memory=False)
 df_raw = df.sample(n=10000)
 
+# save in mysql
 df_name = "raw_data"
 ut.save_to_mysql(db_connect, df_raw, df_name)
+
